fc2f743386d73ddc1bd15c094bcdc19498a724c4
AristotleKandylas <107751800+AristotleKandylas@users.noreply.github.com>
2023-02-07 16:11:53 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
 def test(param1, param2, param3):
   param4 = param1+param3
-  print (param1+param2+param4)
+  param5=param1+param4
+  print ("the final result is: %s" % param5)
+
+
+test(1,2,3)
+  
   
